b491fb04834b4c8ed0f4db1fcfdd403f156337cc
nelzkie <nelson.altares@gmail.com>
2018-01-23 20:01:11 +1300

    added create User UI

---
+++ src/main/java/com/example/skadush/smackchatapp/CreateUserActivity.kt
@@ -0,0 +1,23 @@
+package com.example.skadush.smackchatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarImageViewClcik(view : View){
+
+    }
+    fun createUserButton(view:View){
+
+    }
+    fun generateColorClick(view:View){
+
+    }
+}
--- src/main/java/com/example/skadush/smackchatapp/LogInActivity.kt
+++ src/main/java/com/example/skadush/smackchatapp/LogInActivity.kt
@@ -1,5 +1,6 @@
 package com.example.skadush.smackchatapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LogInActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserButtonClick(view:View){
-
+        val createIntent = Intent(this,CreateUserActivity::class.java)
+        startActivity(createIntent)
     }
 }
